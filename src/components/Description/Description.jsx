199df53ffb2bc0497074aadcc27f7b4834eaaecd
krazzington <mkrazbookings@gmail.com>
2022-05-15 22:37:58 -0400

    styling for tablet mostly done

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -11,15 +11,15 @@ function Description(props) {
     return (
         <>
             <section className="description">
-                <h2 className="h2Header">{videoData.title}</h2>
-                <div className="detailsDiv">
-                    <div className="nameDate">
-                        <p className="nameDate__name"> by {videoData.channel}</p>
-                        <p className="nameDate__date">{dateGenerator(videoData.timestamp)}</p>
+                <h2 className="description__h2Header">{videoData.title}</h2>
+                <div className="description__detailsDiv">
+                    <div className="detailsDiv__nameDate">
+                        <p className="detailsDiv__nameDate--name"> by {videoData.channel}</p>
+                        <p className="detailsDiv__nameDate--date">{dateGenerator(videoData.timestamp)}</p>
                     </div>
-                    <div className="viewsLikes">
-                        <p className="viewsLikes__views"><img src={viewsIcon} alt="views icon" />{videoData.views}</p>
-                        <p className="viewsLikes__likes"><img src={likesIcon} alt="likes icon" />{videoData.likes}</p>
+                    <div className="detailsDiv__viewsLikes">
+                        <p className="detailsDiv__viewsLikes--views"><img src={viewsIcon} alt="views icon" />{videoData.views}</p>
+                        <p className="detailsDiv__viewsLikes--likes"><img src={likesIcon} alt="likes icon" />{videoData.likes}</p>
                     </div>
                 </div>
                 <div className="descriptionDiv">
